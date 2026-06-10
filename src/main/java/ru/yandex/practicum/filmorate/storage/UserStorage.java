19682d535ee907cdfc5f03a69e5c64f82d955054
Ирина Черняева <iridache@192.168.1.4>
2026-06-10 15:32:53 +0300

    add lines

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -5,8 +5,12 @@ import java.util.Collection;
 
 public interface UserStorage {
     User create(User user);
+
     User update(User user);
+
     User delete(int id);
+
     User getById(int id);
+    
     Collection<User> getAll();
 }
